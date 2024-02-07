c388e3990964ddceaba09809dbbfa61e05b23ae8
Victor Maciel Pinheiro <Promissevm.st@gmail.com>
2024-02-07 00:40:14 -0300

    this is the begginig of type script

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import { useEffect, useState, createContext } from 'react';
-import styles from'./App.css';
-import {Home} from './pages/Home'
-import { Contact } from './pages/Contact';
-import { Form } from './pages/Form';
-import { Navbar } from './pages/Navbar';
-import { BrowserRouter as Router, Routes, Route , Link} from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export const AppContext = createContext();
+import styles from'./App.css';
+import { Person } from './components/Person';
 
 
 
 function App() {
-  const client = new QueryClient();
+
   return (
-    <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-        <Navbar/>
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/form' element={<Form/>}/>
-            <Route path='/contact' element={<Contact/>}/>
-            <Route path='*' element={<h1>Page not found</h1>}/>
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+    <div className='App'>
+      <Person
+        name="Pedro"
+        email="pedro@gmail.com"
+        age={21}
+        isMarried={true}
+        friends = {["Jessica", "jake", "jerry", "jasmine"]}
+      />
     </div>
   );
 }
 
+//4:28:08
 export default App;
--- src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query"
-import Axios from "axios"
-
-export const Home = ()=>{
-
-    return (<h1>this is the home page</h1>)
-}
